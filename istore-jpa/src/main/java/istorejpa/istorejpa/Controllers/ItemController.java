1ba09973c3301eb51b617ba4d8ed06e798af9838
abbasferrari <abbas.ferrari@gmail.com>
2021-07-03 15:00:09 +0530

    Update ItemController.java
    Changing the cross origin request url with my localhost publc ip address for testing purposes.
    Revert back once the testing is done.

--- istore-jpa/src/main/java/istorejpa/istorejpa/Controllers/ItemController.java
+++ istore-jpa/src/main/java/istorejpa/istorejpa/Controllers/ItemController.java
@@ -17,7 +17,8 @@ import istorejpa.istorejpa.Repo.ItemRepo;
 import istorejpa.istorejpa.Repo.UserRepo;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://117.216.103.253:4200")
 public class ItemController {
 
 @Autowired
