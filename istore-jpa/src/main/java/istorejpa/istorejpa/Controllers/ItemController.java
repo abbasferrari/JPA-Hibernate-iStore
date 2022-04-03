864528b8b1dceb3e2735cf880a1e1ec3076265e1
abbasferrari <abbas.ferrari@gmail.com>
2022-04-03 14:15:25 +0530

    Update ItemController.java

--- istore-jpa/src/main/java/istorejpa/istorejpa/Controllers/ItemController.java
+++ istore-jpa/src/main/java/istorejpa/istorejpa/Controllers/ItemController.java
@@ -17,7 +17,7 @@ import istorejpa.istorejpa.Repo.ItemRepo;
 import istorejpa.istorejpa.Repo.UserRepo;
 
 @RestController
-//@CrossOrigin(origins = "https://istore-angular.herokuapp.com")
+@CrossOrigin(origins = "https://istore-angular.herokuapp.com")
 public class ItemController {
 
 @Autowired
